aa2ffc6c2ec2698a09fb8362cf97faabc9480f7e
vaporstack <andrew@vaporstack.org>
2018-03-21 16:53:36 -0400

    demo infra

--- demo/src/primitives.h
+++ demo/src/primitives.h
@@ -11,21 +11,7 @@
 
 #include <wsh/wsh.h>
 
-void d_color(double r, double g, double b, double a);
-
-
 //	wash primitives
-
-void d_init(void);
-void d_setup_projection(int w, int h);
-
-void d_rect_w(WRect);
-void d_rect(float, float, float, float);
-void d_rect_wp(WPoint a, WPoint b);
-void d_square(float);
-void d_square_at(float, float, float);
-void d_circle(float);
-
 void d_verts(WLine*);
 void d_wobject_verts(WObject* obj);
 void d_wline_fill(WLine* line);
