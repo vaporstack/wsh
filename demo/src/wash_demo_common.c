998d7b93008b6244c4148fe8e9b42acfd6d07251
rumin4nt <andrew@vaporstack.org>
2018-03-21 20:50:19 -0400

    common

---
+++ demo/src/wash_demo_common.c
@@ -0,0 +1,11 @@
+//
+//  wash_demo_common.c
+//  wash-demo
+//
+//  Created by vs on 3/21/18.
+//  Copyright © 2018 ruminant. All rights reserved.
+//
+
+#include "wash_demo_common.h"
+
+bool wash_demo_has_pressure_sensitivity_of_any_kind = false;
