e2df4635d2847223017b5c023815e4daac1cfaaa
rumin4nt <andrew@vaporstack.org>
2018-04-02 12:26:36 -0400

    important fix for w_sequence

--- src/geo/w_sequence.c
+++ src/geo/w_sequence.c
@@ -12,7 +12,7 @@
 
 void w_sequence_ensure_frame(WSequence* seq)
 {
-
+	
 	int pos = seq->current_frame_index;
 	int adj = pos;
 	if (pos == seq->num_frames) {
@@ -26,7 +26,7 @@ void w_sequence_ensure_frame(WSequence* seq)
 		printf("Adjusted bad frame!\n");
 	}
 	seq->current_frame = seq->frames[seq->current_frame_index];
-
+	
 	/*
 	 if (seq->current_frame_index < 0) {
 	 seq->current_frame_index = seq->num_frames - 1;
@@ -38,7 +38,7 @@ void w_sequence_ensure_frame(WSequence* seq)
 	 if (seq->current_frame_index == seq->num_frames) {
 	 seq->current_frame_index = 0;
 	 }
-
+	 
 	 seq->current_frame = seq->frames[seq->current_frame_index];
 	 printf("Set object frame %d %p\n", seq->current_frame_index,
 	 seq->current_frame);
@@ -47,14 +47,13 @@ void w_sequence_ensure_frame(WSequence* seq)
 
 static void _check_realloc(WSequence* seq)
 {
-
+	
 	if (seq->num_frames == seq->reserved) {
 		seq->reserved *= 2;
-		seq->frames =
-		    realloc(seq->frames, sizeof(WObject*) * seq->reserved);
-
+		seq->frames=realloc(seq->frames, sizeof(WObject*) * seq->reserved);
+		
 		for (int i = seq->current_frame_index; i < seq->reserved; ++i) {
-			seq->frames[i] = NULL;
+			seq->frames[i] = w_object_create(NULL);
 		}
 		printf("Frame realloc! %d\n", seq->reserved);
 	}
@@ -63,7 +62,7 @@ static void _check_realloc(WSequence* seq)
 WSequence* w_sequence_create()
 {
 	WSequence* seq = malloc(sizeof(WSequence));
-
+	
 	// seq->frames = calloc(seq->reserved, sizeof(WObject));
 	seq->num_frames		 = 0;
 	seq->anim_type		 = -1;
@@ -77,27 +76,27 @@ WSequence* w_sequence_create()
 	seq->num_golden_frames   = 0;
 	seq->bounds.pos.x = seq->bounds.size.x = 0;
 	seq->bounds.pos.y = seq->bounds.size.y = 0;
-
+	
 	seq->frames = calloc(seq->reserved, sizeof(WObject*));
-
+	
 	/*
-	seq->num_golden_frames = 3;
-	seq->golden_frames = malloc(sizeof(int) * 4);
-
-	seq->golden_frames[0] = 2;
-	seq->golden_frames[1] = 8;
-	seq->golden_frames[2] = 221345;
+	 seq->num_golden_frames = 3;
+	 seq->golden_frames = malloc(sizeof(int) * 4);
+	 
+	 seq->golden_frames[0] = 2;
+	 seq->golden_frames[1] = 8;
+	 seq->golden_frames[2] = 221345;
 	 */
-
+	
 	w_sequence_frame_create(seq, -1);
-
+	
 	return seq;
 }
 
 WSequence* w_sequence_copy(WSequence* old)
 {
 	WSequence* seq = calloc(1, sizeof(WSequence));
-
+	
 	// seq->frames = calloc(seq->reserved, sizeof(WObject));
 	seq->num_frames		 = old->num_frames;
 	seq->anim_type		 = old->anim_type;
@@ -109,7 +108,7 @@ WSequence* w_sequence_copy(WSequence* old)
 	seq->num_golden_frames   = old->num_golden_frames;
 	seq->bounds.pos.x = seq->bounds.size.x = 0;
 	seq->bounds.pos.y = seq->bounds.size.y = 0;
-
+	
 	seq->frames = calloc(seq->reserved, sizeof(WObject*));
 	for (int i = 0; i < seq->num_frames; i++) {
 		seq->frames[i] = w_object_copy(old->frames[i]);
@@ -118,14 +117,14 @@ WSequence* w_sequence_copy(WSequence* old)
 	/*
 	 seq->num_golden_frames = 3;
 	 seq->golden_frames = malloc(sizeof(int) * 4);
-
+	 
 	 seq->golden_frames[0] = 2;
 	 seq->golden_frames[1] = 8;
 	 seq->golden_frames[2] = 221345;
 	 */
-
+	
 	// w_sequence_frame_create(seq, -1);
-
+	
 	return seq;
 }
 
@@ -135,21 +134,21 @@ void w_sequence_frame_create(WSequence* seq, int pos)
 	// OLD AND BAD BUT IT FUCKING WORKED!?
 	if (pos == -1) {
 		seq->num_frames++;
-
+		
 		_check_realloc(seq);
-
+		
 		// printf("creating frame at %d\n", seq->num_frames);
-
+		
 		WObject* obj			 = w_object_create(NULL);
 		seq->frames[seq->num_frames - 1] = obj;
 		seq->current_frame_index	 = seq->num_frames - 1;
-
+		
 		//
 		seq->current_frame = seq->frames[seq->current_frame_index];
-
+		
 	} else {
 		// printf("creating frame at %d\n", pos);
-
+		
 		assert(false);
 		printf("IMPLEMENT THIS!\n");
 	}
@@ -165,30 +164,30 @@ void w_sequence_frame_set(WSequence* seq, int ind)
 		printf("I _really_ can't do that dave\n");
 		return;
 	}
-
+	
 	seq->current_frame_index = ind;
 	seq->current_frame       = seq->frames[ind];
-
+	
 	w_sequence_ensure_frame(seq);
 }
 
 void w_sequence_frame_add(WSequence* seq)
 {
-
+	
 	// seq->current_frame_index;
 	seq->num_frames++;
-
+	
 	_check_realloc(seq);
-
+	
 	WObject* fr = w_object_create(NULL);
-
+	
 	if (seq->current_frame_index < seq->num_frames - 1)
 		seq->current_frame_index++;
-
+	
 	seq->frames[seq->current_frame_index] = fr;
 	printf("added frame %d (%d)\n", seq->current_frame_index,
 	       seq->num_frames);
-
+	
 	// seq->current_frame_index = cfi;
 	seq->current_frame = seq->frames[seq->current_frame_index];
 	
@@ -199,25 +198,25 @@ void w_sequence_frame_add(WSequence* seq)
 
 void w_sequence_frame_duplicate(WSequence* seq)
 {
-
+	
 	// int num = seq->num_frames;
 	int pos = seq->current_frame_index;
-
+	
 	seq->num_frames++;
-
+	
 	printf("duplicate frame at %d\n", pos);
-
+	
 	_check_realloc(seq);
-
+	
 	WObject* orig = seq->frames[pos];
 	//	new thingy
 	WObject* cpy = w_object_copy(orig);
-
+	
 	// move everything after n up
 	for (int i = seq->num_frames; i > pos; --i) {
 		seq->frames[i] = seq->frames[i - 1];
 	}
-
+	
 	seq->frames[pos] = cpy;
 	pos++;
 	seq->current_frame_index = pos;
@@ -227,24 +226,24 @@ void w_sequence_frame_duplicate(WSequence* seq)
 
 void w_sequence_frame_insert(WSequence* seq)
 {
-
+	
 	// int num = seq->num_frames;
 	int pos = seq->current_frame_index;
-
+	
 	seq->num_frames++;
-
+	
 	printf("insert frame at %d\n", pos);
 	seq->frames = realloc(seq->frames, sizeof(WObject*) * seq->num_frames);
-
+	
 	// WObject* orig = seq->frames[pos];
 	//	new thingy
 	// WObject* cpy = w_object_copy(orig);
-
+	
 	// move everything after n up
 	for (int i = seq->num_frames; i > pos; --i) {
 		seq->frames[i] = seq->frames[i - 1];
 	}
-
+	
 	seq->frames[pos] = w_object_create(NULL);
 	pos++;
 	seq->current_frame_index = pos - 1;
@@ -254,9 +253,9 @@ void w_sequence_frame_insert(WSequence* seq)
 
 void w_sequence_frame_delete(WSequence* seq)
 {
-
+	
 	int pos = seq->current_frame_index;
-
+	
 	int num = seq->num_frames;
 	if (num <= 1) {
 		printf("Can't delete last frame :O\n");
@@ -287,23 +286,23 @@ void w_sequence_destroy(WSequence* seq)
 }
 void w_sequence_frame_next(WSequence* seq)
 {
-
+	
 	if (!seq)
 		return;
-
+	
 	if (seq->num_frames == 0) {
 		printf("can't do next frame, no frames!\n");
 		return;
 	}
 	seq->current_frame_index++;
 	/*
-	if (seq->current_frame_index == seq->num_frames) {
-		seq->current_frame_index = 0;
-	}
-
-	seq->current_frame = seq->frames[seq->current_frame_index];
-	printf("Set object frame %d %p\n", seq->current_frame_index,
-	       seq->current_frame);
+	 if (seq->current_frame_index == seq->num_frames) {
+	 seq->current_frame_index = 0;
+	 }
+	 
+	 seq->current_frame = seq->frames[seq->current_frame_index];
+	 printf("Set object frame %d %p\n", seq->current_frame_index,
+	 seq->current_frame);
 	 */
 	w_sequence_ensure_frame(seq);
 }
@@ -312,19 +311,19 @@ void w_sequence_frame_prev(WSequence* seq)
 {
 	if (!seq)
 		return;
-
+	
 	if (seq->num_frames == 0) {
 		printf("can't do next frame, no frames!\n");
 		return;
 	}
-
+	
 	seq->current_frame_index--;
 	/*
 	 if (seq->current_frame_index < 0) {
-		seq->current_frame_index = seq->num_frames - 1;
-	}
-	seq->current_frame = seq->frames[seq->current_frame_index];
-*/
+	 seq->current_frame_index = seq->num_frames - 1;
+	 }
+	 seq->current_frame = seq->frames[seq->current_frame_index];
+	 */
 	w_sequence_ensure_frame(seq);
 }
 
@@ -340,8 +339,8 @@ void w_sequence_normalize_time_continuous(WSequence* seq)
 	//return;
 	
 	//for (int i = 0; i < seq->num_frames; ++i) {
-		//WObject* fr = seq->frames[i];
-
+	//WObject* fr = seq->frames[i];
+	
 	//}
 }
 
@@ -371,7 +370,7 @@ void w_sequence_normalize(WSequence* seq)
 		WObject* o = seq->frames[i];
 		w_object_calc_bounds(o);
 	}
-
+	
 	double minx, miny, maxx, maxy;
 	
 	minx = miny = INFINITY;
@@ -379,17 +378,17 @@ void w_sequence_normalize(WSequence* seq)
 	//miny = INFINITY;
 	//maxy = -INFINITY;
 	//WObject* first = seq->frames[0];
-
+	
 	//minx = maxx = first->bounds.pos.x;
 	// miny = maxy = first->bounds.pos.y;
-
+	
 	for (int i = 0; i < seq->num_frames; ++i) {
 		WObject* fr = seq->frames[i];
 		double   x1 = fr->bounds.pos.x;
 		double   y1 = fr->bounds.pos.y;
 		double   x2 = fr->bounds.pos.x + fr->bounds.size.x;
 		double   y2 = fr->bounds.pos.y + fr->bounds.size.y;
-
+		
 		if ( x1 < minx )
 			minx = x1;
 		if ( x2 > maxx)
@@ -399,35 +398,35 @@ void w_sequence_normalize(WSequence* seq)
 		if ( y2 > maxy)
 			maxy = y2;
 	}
-
+	
 	seq->bounds.pos.x  = minx;
 	seq->bounds.pos.y  = miny;
 	seq->bounds.size.x = maxx - minx;
 	seq->bounds.size.y = maxy - miny;
 	/*
-	if ( miny > maxy )
-	{
-		SWAP(double, miny, maxy);
-	}
-	if ( minx > maxx )
-	{
-		SWAP(double, minx, maxx);
-	}
-	*/
+	 if ( miny > maxy )
+	 {
+	 SWAP(double, miny, maxy);
+	 }
+	 if ( minx > maxx )
+	 {
+	 SWAP(double, minx, maxx);
+	 }
+	 */
 	
 	double dx = maxx - minx;
 	double dy = maxy - miny;
-
+	
 	// double cx = minx + (.5 * (maxx - minx));
 	// double cy = miny + (.5 * (maxy - miny));
-
+	
 	// double ar = fabs(dy /dx);
 	
 	seq->transform.scale.x = dx;
 	seq->transform.scale.y = dy;
 	for (int i = 0; i < seq->num_frames; ++i) {
 		WObject* fr = seq->frames[i];
-
+		
 		for (int j = 0; j < fr->num_lines; ++j) {
 			WLine* l = fr->lines[j];
 			for (unsigned long long k = 0; k < l->num; ++k) {
