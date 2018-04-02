c4fb028899d56aa304c1761ccc4c606176645e36
vs <andrew@vaporstack.org>
2018-04-02 14:44:34 -0400

    more demo stubs

---
+++ demo/src/demos/demo.c
@@ -0,0 +1,20 @@
+
+#include "demo.h"
+
+#include "simulator.h"
+#include "cel_animation.h"
+#include "procedural_brush.h"
+#include "dynamic_resize.h"
+#include "realtime_playback.h"
+#include "wsh_session.h"
+
+
+int demos_init(void)
+{
+	return 0;
+}
+
+int demos_deinit(void)
+{
+	return 0;
+}
---
+++ demo/src/demos/wsh_session.c
@@ -0,0 +1,14 @@
+//
+//  wsh_session.c
+//  wash-demo
+//
+//  Created by vs on 3/31/18.
+//  Copyright © 2018 ruminant. All rights reserved.
+//
+
+#include "wsh_session.h"
+
+
+//	demonsstration of using the wash session recording functionality
+
+
