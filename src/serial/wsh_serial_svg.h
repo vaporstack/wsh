6dc1e785bd1f7463dd2fb8d23b73283e08bf9943
vaporstack <andrew@vaporstack.org>
2018-06-18 18:40:17 -0400

    svg (cairo) infra

---
+++ src/serial/wsh_serial_svg.h
@@ -0,0 +1,23 @@
+
+//  w_serial_svg.c
+//  w_serial_svg
+//
+//  Created by Andrew Macfarlane on 21/03/17.
+//  Copyright © 2017 vaporstack. All rights reserved.
+
+#ifndef w_serial_svg_h_
+#define w_serial_svg_h_
+
+
+#define WSH_ENABLE_SERIAL_BACKEND_SVG
+
+
+#ifdef WSH_ENABLE_SERIAL_BACKEND_SVG
+
+#include <wsh/wsh.h>
+
+const char* wsh_serial_svg_document_serialize(WDocument*);
+WDocument*  wsh_serial_svg_document_unserialize(const char* path);
+#endif
+
+#endif
