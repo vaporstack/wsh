5ade6b4b153f7c5747a3f3ea9867ca2c6e427a03
vaporstack <andrew@vaporstack.org>
2018-12-14 17:57:54 -0500

    build fixes for gpc ext

--- src/ext/wsh_gpc.c
+++ src/ext/wsh_gpc.c
@@ -10,53 +10,48 @@
 
 #ifdef WSH_ENABLE_EXT_GPC
 
-#include <deps/gpc/gpc.h>
-
+#include "../../contrib/gpc/gpc.h"
 
 void* wsh_ext_gpc_tess_create_wline(WLine* line)
 {
-	
+
 	// printf("Creating tesselation!\n");
-	
+
 	gpc_polygon poly;
 	gpc_vertex* verts;
 	verts = calloc(line->num, sizeof(gpc_vertex));
 	gpc_vertex_list contour;
-	
+
 	//WLine* line = brush->hnd.src;
-	
+
 	for (unsigned long long i = 0; i < line->num; ++i)
 	{
 		WPoint p   = line->data[i];
 		verts[i].x = p.x;
 		verts[i].y = p.y;
 	}
-	
+
 	gpc_tristrip* tstrips = calloc(1, sizeof(gpc_tristrip));
 	contour.vertex	= verts;
 	contour.num_vertices  = line->num;
 	poly.contour	  = &contour;
-	
+
 	poly.num_contours = 1;
 	poly.hole	 = NULL;
-	
+
 	// tstrip_num = tstrips.num_strips;
 	// r_color(.5,0,0,.5);
 	// drw_gpc_verts(poly);
-	
+
 	// gpc_tristrip* loc = tstrips;
-	
+
 	gpc_polygon_tr_tristrip(&poly, tstrips);
 	return tstrips;
 	//line->tess = tstrips;
 	// line->tess = (void*)tstrips;
 	// r_push();
-	
+
 	// r_translate_x(-128);
 }
 
-
-
-
 #endif
-
