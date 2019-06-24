a615b6364beafc056689bc47824762321bf77401
vaporstack <andrew@vaporstack.org>
2019-06-24 17:33:26 -0400

    operators and tvos project

---
+++ wsh_config.h
@@ -0,0 +1,14 @@
+//
+//  wsh_config.h
+//  wsh
+//
+//  Created by vs on 6/1/19.
+//  Copyright © 2019 vaporstack. All rights reserved.
+//
+
+#ifndef wsh_config_h
+#define wsh_config_h
+
+#define WSH_ENABLE_SERIAL_BACKEND_JSON
+ 
+#endif /* wsh_config_h */
